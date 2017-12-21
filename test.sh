8b4293c6a777dd58c9d8e4bda27b908af7285019
localstepdo <stepdo@microsoft.com>
2017-12-21 10:43:14 -0800

    systemd docker commands

--- test.sh
+++ test.sh
@@ -2,5 +2,5 @@
 # Licensed under the MIT license. See LICENSE file on the project webpage for details.
 
 # Rely on command's return result
-set -e
+set -ex
 systemctl > /dev/null && echo "success!"
