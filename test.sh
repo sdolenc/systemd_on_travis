71182967cf49a1a6b9d7d78d9f41db711644de48
localstepdo <stepdo@microsoft.com>
2017-12-21 11:19:09 -0800

    connect to container

--- test.sh
+++ test.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 # Licensed under the MIT license. See LICENSE file on the project webpage for details.
 
+# Determine the appropriate github branch to clone using Travis environment variables
+BRANCH=${TRAVIS_PULL_REQUEST_BRANCH:-$TRAVIS_BRANCH}
+echo "BRANCH=$BRANCH"
+
+docker exec -i stepdo0 /bin/bash -s <<EOF
+
 # Rely on command's return result
 set -ex
+
 systemctl > /dev/null && echo "success!"
